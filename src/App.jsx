ff24d8bf89411740da02118348cc0bd4ce5edd88
AyAitch271 <154345991+AyAitch271@users.noreply.github.com>
2025-06-11 15:59:09 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  useEffect(() => {
+    // fetching products
+  }, [])
+
+  const [products, setProducts] = useState([]);
+
+  const [cart, setCart] = useState([]) 
+
+  const [isVisible, setIsVisible] = useState(false)
+
+  const addToCart = (id) => {
+
+  }
+
+  const removeFromCart = (id) => {
+
+  }
+
+  const updateCart = () => {
+    
+  }
+
+  const clearCart = () => {
+    
+  }
+
+  const increment = (id) => {
+
+  }
+
+  const decrement = (id) => {
+
+  }
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
